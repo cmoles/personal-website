690f24a78daf0e6ecb7ec08ba9abd71a0f4cc3fd
Chris Moles <cmoles@gmail.com>
2023-08-09 14:19:01 -0700

    fix build error

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function get() {
+	const blog = await getCollection('blog');
 	return rss({
 		title: 'Chris Moles | Blog',
-		description: 'My journey learning Astro',
+		description: 'My wonderful, personal website.',
 		site: 'https://chris-moles.netlify.app',
-		items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			url: `https://chris-moles.netlify.app/blog/${post.slug}`,
+			link: `/blog/${post.slug}`,
+		})),
 		customData: `<language>en-us</language>`,
 	});
 }
