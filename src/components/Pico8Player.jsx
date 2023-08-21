200f2276610d5b6f4b341475bd63012626cbf398
Chris Moles <cmoles@gmail.com>
2023-08-21 10:16:23 -0700

    add media query to pico player

--- src/components/Pico8Player.jsx
+++ src/components/Pico8Player.jsx
@@ -9,6 +9,28 @@ import { Controls,
 import 'react-pico-8/styles.css'
 import {isMobile} from 'react-device-detect';
 
+export const MobileLink = ({cart}) => {
+	const [mQuery, setMQuery] = React.useState({
+		matches: window.matchMedia("(max-width: 600px)").matches,
+	});
+
+	React.useEffect(() => {
+		let mediaQuery = window.matchMedia("(max-width: 600px)");
+		mediaQuery.addListener(setMQuery);
+
+		return () => mediaQuery.removeListener(setMQuery);
+	}, []);
+
+	if (mQuery && mQuery.matches) {
+		return (
+			<p>
+				<a href={cart+".html"}>Click here to play on mobile.</a>
+			</p>
+		);
+	}
+	return (<></>);
+}
+
 export const Pico8Player = ({cart,placeholder}) => {
 
 	if (isMobile) {
@@ -17,6 +39,8 @@ export const Pico8Player = ({cart,placeholder}) => {
 		)
 	}
 	return (
+		<>
+		<MobileLink cart={cart} />
 		<Pico8 src={cart+".js"}
 				autoPlay={true}
 				legacyButtons={false}
@@ -33,5 +57,6 @@ export const Pico8Player = ({cart,placeholder}) => {
 			<Sound/>
 			<Fullscreen/>
 		</Pico8>
+		</>
 	)
 }
